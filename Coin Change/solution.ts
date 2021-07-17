93b5b61573896444c50b438c31b9297bc780e31e
luisgepeto <luisgepeto@users.noreply.github.com>
2021-07-17 12:03:49 -0500

    updated coin problem solution

--- Coin Change/solution.ts
+++ Coin Change/solution.ts
@@ -1,39 +1,21 @@
-const getSmallestCoin = (
-  sortedCoins: number[],
-  amount: number
-): number | null => {
-  let smallestCoin: number | null = null;
-  sortedCoins.some((currentCoin) => {
-    if (currentCoin <= amount) {
-      smallestCoin = currentCoin;
-      return true;
-    }
-    return false;
-  });
-  return smallestCoin;
-};
-
-const getCoinChange = (
-  sortedCoins: number[],
-  amount: number,
-  countCoins: number
-): number => {
-  const smallestCoin = getSmallestCoin(sortedCoins, amount);
-  if (smallestCoin === null) return -1;
-  if (smallestCoin === amount) return 1;
-  sortedCoins.map((c) => {
+const getCoinChange = (coins: number[], amount:number, cacheCoins:{}):number =>{
+  if(amount == 0) return 0;
+  if(amount < 0) return -1;  
+  if(cacheCoins[amount] !== undefined) return cacheCoins[amount];
+  let minCoinCount = Number.MAX_SAFE_INTEGER;
+  coins.map((c) => {
     const remainingAmount = amount - c;
-    countCoins++;
-    if (getCoinChange(sortedCoins, remainingAmount, countCoins) === -1)
-      countCoins = -1;
+    const coinCount = getCoinChange(coins, remainingAmount, cacheCoins);
+    if (coinCount !== -1){
+      minCoinCount = Math.min(minCoinCount, coinCount+1);
+    }      
   });
-  return countCoins;
-};
-
-function coinChange(coins: number[], amount: number): number {
-  if (amount === 0) return 0;
-  const sortedCoins = coins.sort().reverse();
-  return getCoinChange(sortedCoins, amount, 0);
+  cacheCoins[amount] = minCoinCount === Number.MAX_SAFE_INTEGER ? -1: minCoinCount;
+  return cacheCoins[amount];
 }
 
-console.log(coinChange([6, 5, 1], 16));
+function coinChange(coins: number[], amount: number): number {
+  return getCoinChange(coins, amount, {});
+}
+
+console.log(coinChange([2], 3));
